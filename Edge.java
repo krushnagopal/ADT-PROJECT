c7a22dd51299a4ea816aedd3f448b818ac826172
krushnagopal <krushnagopal785@gmail.com>
2022-02-17 22:54:09 -0500

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -2,18 +2,20 @@ import java.util.Collections;
 import java.util.Comparator;
 // compartor class
 
-
+/* Edge graph with its properties*/
 public class Edge {
   private Vertex start;
   private Vertex end;
   public boolean block;
   public String color;
   
+  /* Edge constructor */
   public Edge(Vertex startV, Vertex endV)  {
     this.start = startV;
     this.end = endV;
     
   }
+  /* Edge constructor */
   public Edge(Vertex startV, Vertex endV, String color)  {
 	    this.start = startV;
 	    this.end = endV;
@@ -21,10 +23,11 @@ public class Edge {
 	    this.color = color;
 	  }
     
+  /* Starting vertex */
   public Vertex getStart() {
     return this.start;
   }
-    
+  /* Ending vertex */
   public Vertex getEnd() {
     return this.end;
   }
